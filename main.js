f46fea07d73cbfdfcee99ff6065ae2f738af1e5a
Alec Jordan <alecbjordan@gmail.com>
2022-01-03 21:35:17 -0500

    Add assets and twitter embed

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            about_me: "My name is Alec Jordan"
+            about_me: "My name is Alec Jordan. I am a Web Developer pursuing a career in Level Design. I like making youtube videos and web-apps, skiing, and playing video and board games with my friends! I graduated with a degree in Computer Science from Northeastern University in 2018 and have been figuring things out ever since.",
+            bio_photo_link: 'assets/images/hot dog.jpg',
+
         }
     },
     methods: {
